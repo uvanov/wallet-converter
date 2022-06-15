9205981a772393b70eb8738fd7bca4c39acfc4b6
uvanov <kirill.ivanov000@gmail.com>
2022-06-15 19:45:43 +0300

    Uninstall Redux [neadless :D]

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,8 @@ import { App } from './App'
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 
-// Redux Store
-import { Provider } from 'react-redux'
-import { store } from './store/store';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </ChakraProvider>
 );
