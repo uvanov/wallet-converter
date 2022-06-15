dca5db06a9a1999c9a5cb8661591d04fd1de854b
uvanov <kirill.ivanov000@gmail.com>
2022-06-15 19:40:55 +0300

    Remove ReactStrict mode

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,9 @@ import { Provider } from 'react-redux'
 import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ChakraProvider>
-  </React.StrictMode>
-)
+  <ChakraProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ChakraProvider>
+);
